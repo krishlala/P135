090defbccb797b17420516b869cd1ea92d3b3ac7
krishlala <71400038+krishlala@users.noreply.github.com>
2021-11-13 13:26:43 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+video = "";
+objects = [];
+
 function setup() {
   canvas = createCanvas(600, 500);
   canvas.center();
@@ -5,8 +8,22 @@ function setup() {
   video.hide();
 }
 
-function draw() {
-  image(video, 0, 0, 600, 500);
+function draw(){
+    image(video, 0, 0, 600, 500);
+    if(status != "") {
+        objectDetector.detect(video, gotResult);
+        for(i=0; i < objects.length; i++) {
+            document.getElementById("status").innerHTML = "Status : Objects Detected";
+            document.getElementById("num_obj").innerHTML = "Number of objects detected are: "+objects.length;
+
+            fill("red");
+            percent=floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%", objects[i].x +15, objects[i].y +15);
+            noFill();
+            stroke("red");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+    }
 }
 
 function start() {
@@ -18,3 +35,13 @@ function modelLoaded() {
 console.log("Model Loaded");
   status = true;
 }
+
+
+function gotResult(error, results) {
+    if(error) {
+        console.error(error);
+    } else {
+        console.log(results);
+        objects = results;
+    }
+} 
